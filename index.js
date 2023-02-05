069577569c0fe4ef2099294506911922294b64b6
James Bennett <jimmyb1014@hotmail.com>
2023-02-05 17:52:42 -0600

    add html and styles, soft coded js

---
+++ index.js
@@ -0,0 +1,12 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+//loop through the array
+for (let i = 0; i < characters.length; i ++) {
+  let index = Math.floor(Math.random * characters.length)
+  return index
+}
+console.log(index[i])
+//randomly select a character
+//display character in passwordEl
+//repeat 15 times (once for each password character)
