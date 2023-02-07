8cf8df725b4a08e94924193bfbd6916cafe6bee6
James Bennett <jimmyb1014@hotmail.com>
2023-02-06 18:48:37 -0600

    Javascript working, app functional, no stretch goals

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
+let passwordElOne = document.querySelector("#password-el-one")
+let passwordElTwo = document.querySelector("#password-el-two")
+
 
 //loop through the array
-for (let i = 0; i < characters.length; i ++) {
-  let index = Math.floor(Math.random * characters.length)
-  return index
+//randomly generate index number
+//generate character with index number
+function clearPassword() {
+    passwordElOne.textContent = ""
+    passwordElTwo.textContent = ""
+    generatePassword()
 }
-console.log(index[i])
-//randomly select a character
-//display character in passwordEl
-//repeat 15 times (once for each password character)
+
+function generatePassword() {
+    for (let i = 0; i < 15; i++) {
+     let = passwordOneIndex = Math.floor( Math.random() * characters.length )
+     let = passwordTwoIndex = Math.floor( Math.random() * characters.length )
+        
+    passwordElOne.textContent += characters[passwordOneIndex]
+    passwordElTwo.textContent += characters[passwordTwoIndex]
+    }
+}
+
+function copyToClipboard() {
+    passwordElOne.select()
+    navigator.clipboard.writeText(passwordElOne.value)
+    alert("Password copied" + passwordElOne.value)
+}
